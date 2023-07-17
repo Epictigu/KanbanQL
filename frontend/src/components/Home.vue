54bbc3211d7eade2c350cb0ce3ba3a79eff658c7
Epictigu <tigusa.r@gmail.com>
2023-07-17 20:12:06 +0200

    Board Frontend

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,15 +1,17 @@
 <template>
-    <h2>Mainpage</h2>
+    <NavigationBar/>
     <TicketBoard/>
 </template>
 
 <script lang="ts">
 import {defineComponent} from "vue";
 import TicketBoard from "@/components/TicketBoard.vue";
+import NavigationBar from "@/components/NavigationBar.vue";
 
 export default defineComponent({
     name: "Home",
     components: {
+        NavigationBar,
         TicketBoard
     },
 });
